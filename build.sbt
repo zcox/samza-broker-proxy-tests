e9d70d2a53d993e80333dbd2e08f430bb319eb34
Zach Cox <zach.cox@banno.com>
2015-01-29 19:56:40 -0600

    fixed! logback somehow causes BrokerProxy.stop to block... things work using log4j

--- build.sbt
+++ build.sbt
@@ -14,10 +14,10 @@ libraryDependencies ++= {
     "org.apache.samza" %% "samza-kv-leveldb" % samzaVersion,
     "org.apache.kafka" %% "kafka" % kafkaVersion classifier "test" exclude("log4j", "log4j"),
     "org.apache.kafka" % "kafka-clients" % kafkaClientsVersion,
-    "org.slf4j" % "log4j-over-slf4j" % "1.7.10",
-    "ch.qos.logback" % "logback-classic" % "1.0.13",
-    "org.specs2" %% "specs2" % "2.4.2" % "test",
-    "com.novocode" % "junit-interface" % "0.11" % "test"
+    // "org.slf4j" % "log4j-over-slf4j" % "1.7.10",
+    // "ch.qos.logback" % "logback-classic" % "1.0.13", <============================== this apparently causes BrokerProxy.stop to block, for some reason... .... ..... ... . . ..
+    "log4j" % "log4j" % "1.2.17", //kafka.utils.Logging does a `import org.apache.log4j.Logger` so our code will not even compile with out this #tisktisk
+    "org.specs2" %% "specs2" % "2.4.2" % "test"
   )
 }
 
--- src/test/scala/RestoreStoreSpec.scala
+++ src/test/scala/RestoreStoreSpec.scala
@@ -76,7 +76,7 @@ object RestoreStoreSpec extends Specification {
 
       val (job2, task2) = runJob()
       sendToKafka("input", ("a", 4))
-      getMessages[String, Int]("mystore", 3) must_== Seq(("a", 10))
+      getMessages[String, Int]("mystore", 1) must_== Seq(("a", 10))
 
       println("Stopping job the 2nd time")
       stopJob(job)
@@ -111,8 +111,8 @@ object RestoreStoreSpec extends Specification {
       "systems.kafka.samza.factory" -> "org.apache.samza.system.kafka.KafkaSystemFactory",
       // Always start consuming at offset 0. This avoids a race condition between
       // the producer and the consumer in this test (SAMZA-166, SAMZA-224).
-      "systems.kafka.samza.offset.default" -> "oldest", // applies to a nonempty topic <==================== same results with or without this
-      "systems.kafka.consumer.auto.offset.reset" -> "smallest", // applies to an empty topic <============== same results with or without this
+      // "systems.kafka.samza.offset.default" -> "oldest", // applies to a nonempty topic
+      // "systems.kafka.consumer.auto.offset.reset" -> "smallest", // applies to an empty topic
       // "systems.kafka.samza.msg.serde" -> "string",
       "systems.kafka.consumer.zookeeper.connect" -> zkConnect,
       "systems.kafka.producer.metadata.broker.list" -> metadataBrokerList,
@@ -145,7 +145,7 @@ object RestoreStoreSpec extends Specification {
 
     def stopJob(job: StreamJob): Unit = {
       job.kill
-      SamzaBrokerProxyThreadInterruptHack.interruptAllBrokerProxyThreads() //if this is removed, then the job will not shutdown, and test will fail on next line
+      // SamzaBrokerProxyThreadInterruptHack.interruptAllBrokerProxyThreads() //if this is removed, then the job will not shutdown, and test will fail on next line
       job.waitForFinish(60000) must_== UnsuccessfulFinish
       RestoreStoreSpecTask.tasks.clear()
     }
